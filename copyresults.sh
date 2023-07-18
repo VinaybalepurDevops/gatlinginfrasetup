9f32ddfe4904a6f33a55ebee72e6e7bf2e9a203d
Ubuntu <ubuntu@ip-172-31-94-43.ec2.internal>
2023-07-18 08:46:56 +0000

    Added code to avoid host check message during copy of results file

--- copyresults.sh
+++ copyresults.sh
@@ -4,5 +4,5 @@ a=0
 for (( i=0; i<"$len"; i++ ))
 do
 	ip=$(terraform output -json ip | jq -r .["$i"])
-    scp -r -i terraform_ec2_key ubuntu@"$ip":/home/ubuntu/gatling/gatling-charts-highcharts-bundle-3.9.5/results/resultfolder/lognode*.zip /home/ubuntu/results
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r -i terraform_ec2_key ubuntu@"$ip":/home/ubuntu/gatling/gatling-charts-highcharts-bundle-3.9.5/results/resultfolder/lognode*.zip /home/ubuntu/results
 done
